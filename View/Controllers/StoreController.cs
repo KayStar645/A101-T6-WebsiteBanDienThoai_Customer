640e3ef8340905c18575446daa64e5135e02389c
SonNV <sonnv1912@gmail.com>
2023-11-13 22:50:08 +0700

    feat: add home layout

---
+++ View/Controllers/StoreController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace View.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ILogger<StoreController> _logger;
+
+        public StoreController(ILogger<StoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return PartialView("_Layout");
+        }
+
+        public IActionResult Home()
+        {
+            return PartialView("_Home");
+        }
+    }
+}
